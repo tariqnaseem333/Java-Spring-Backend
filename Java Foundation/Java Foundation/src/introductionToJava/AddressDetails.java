fcebd159b7797461042a8cf0b3cc87defb31496b
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-01-04 20:57:36 +0530

    Learn Introduction to Java

---
+++ Java Foundation/Java Foundation/src/introductionToJava/AddressDetails.java
@@ -0,0 +1,14 @@
+package introductionToJava;
+
+public class AddressDetails {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Door No: D017"); 
+		System.out.println("Street: Wz O/87 Outer Ring Road");
+		System.out.println("City: Delhi");
+		System.out.println("ZIP Code: 390001");
+
+	}
+
+}
---
+++ Java Foundation/Java Foundation/src/introductionToJava/EmployeeDetails.java
@@ -0,0 +1,12 @@
+package introductionToJava;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		
+		System.out.println("My name is Tariq Naseem.");
+		System.out.println("My designation is System Engineer.");
+
+	}
+
+}
