11710dd626b00ea244642890bc132cdacbead68f
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-01-07 20:12:12 +0530

    Solve Armstrong, LuckyNumber and SeedOfNumber problem of iterationControlStructures

---
+++ Java Foundation/Java Foundation/src/controlStructures/iterationControlStructures/SeedOfNumber.java
@@ -0,0 +1,33 @@
+package controlStructures.iterationControlStructures;
+
+import java.util.Scanner;
+
+public class SeedOfNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the seed Number: ");
+		int num1 = scanner.nextInt();
+		System.out.println("Enter the real Number: ");
+		int num2 = scanner.nextInt();
+		
+		int seed = num1;
+		int rem = 0;
+		int N = num1;
+		while( N > 0 ) {
+			rem = N%10;
+			seed = seed*rem;
+			N = N/10;
+		}
+		
+		if( seed == num2 ) {
+			System.out.println(num1 + " is a seed of " + num2);
+		} else {
+			System.out.println(num1 + " is not a seed of " + num2);
+		}
+		
+
+	}
+
+}
