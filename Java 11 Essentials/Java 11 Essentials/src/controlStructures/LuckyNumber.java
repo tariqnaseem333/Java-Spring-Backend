ead923eee1e5e825c93801bb40a77305dcfe7c5d
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-11-14 23:42:09 +0530

    Updated controlStructures package's code files

--- Java 11 Essentials/Java 11 Essentials/src/controlStructures/LuckyNumber.java
+++ Java 11 Essentials/Java 11 Essentials/src/controlStructures/LuckyNumber.java
@@ -15,14 +15,14 @@ public class LuckyNumber {
 		}
 		
 		int rem = 0;
-		int sum = 0;
+		int luckyNumber = 0;
 		while( N > 0 ) {
 			rem = N % 10;
-			sum = sum + (int)Math.pow(rem, 2);
+			luckyNumber = luckyNumber + (int)Math.pow(rem, 2);
 			N = N / 100;
 		}
 		
-		if( sum%9 == 0 ) {
+		if( luckyNumber%9 == 0 ) {
 			System.out.println("The number " + num + " is a Lucky Number");
 		} else {
 			System.out.println("Oops! Not a Lucky number");
