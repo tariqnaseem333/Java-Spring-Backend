24553fe30edd67ad07fd543f18002d4879b84ad5
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-11-14 23:48:22 +0530

    Updated stringClass package's code files

--- Java 11 Essentials/Java 11 Essentials/src/stringClass/Customer.java
+++ Java 11 Essentials/Java 11 Essentials/src/stringClass/Customer.java
@@ -8,17 +8,28 @@ public class Customer{
 	
 //	Methods
 	public void findDiscountAndName(String inputString) {
-		String str = inputString.trim();
-		int n = str.indexOf(":");
-		this.setCustomerName(str.substring(n+1, str.length()));
-		String customerType = str.substring(0, n);
-		if( customerType.equals("Regular") ) {
+		
+		String[] customer = inputString.trim().split(":");
+		this.setCustomerName(customer[1]);
+		if( customer[0].equals("Regular") ) {
 			this.setApplicableDiscount(10);
-		} else if( customerType.equals("Privileged") ) {
+		} else if( customer[0].equals("Privileged") ) {
 			this.setApplicableDiscount(20);
 		} else {
-			this.setApplicableDiscount(0);
+			System.out.println("Invalid Customer Name!!");
 		}
+		
+//		String str = inputString.trim();
+//		int n = str.indexOf(":");
+//		this.setCustomerName(str.substring(n+1, str.length()));
+//		String customerType = str.substring(0, n);
+//		if( customerType.equals("Regular") ) {
+//			this.setApplicableDiscount(10);
+//		} else if( customerType.equals("Privileged") ) {
+//			this.setApplicableDiscount(20);
+//		} else {
+//			this.setApplicableDiscount(0);
+//		}
 	}
 	
 //	Getters and Setters
