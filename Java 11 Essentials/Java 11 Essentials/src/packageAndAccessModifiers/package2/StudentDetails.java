b1ed0e4db7b0e2365ef790199a2b0311e5e17cf0
tariqnaseem333 <tariqnaseem333@gmail.com>
2023-11-14 23:46:14 +0530

    Updated packageAndAccessModifiers package's code files

--- Java 11 Essentials/Java 11 Essentials/src/packageAndAccessModifiers/package2/StudentDetails.java
+++ Java 11 Essentials/Java 11 Essentials/src/packageAndAccessModifiers/package2/StudentDetails.java
@@ -7,15 +7,15 @@ public class StudentDetails {
 //	Methods
 	public void calculatePercentage( Student student ) {
 		double percentage = ( student.getTotalMarksObtained() / student.getTotalMarks() ) * 100.0;
-		System.out.println("Percentage: " + Math.round(percentage*100) / 100.0);
+		System.out.println("	Percentage: " + Math.round(percentage*100) / 100.0 + "%");
 	}
 	
 	public void displayStudentDetails( Student s ) {
 		System.out.println("Student Details:");
-		System.out.println("Student ID: " + s.getStudentId());
-		System.out.println("Student Name: " + s.studentName);
-		System.out.println("Student Marks: " + s.getTotalMarksObtained());
-		System.out.println("Total Marks: " + s.getTotalMarks());
+		System.out.println("	Student ID: " + s.getStudentId());
+		System.out.println("	Student Name: " + s.getStudentName());
+		System.out.println("	Student Marks: " + s.getTotalMarksObtained());
+		System.out.println("	Total Marks: " + s.getTotalMarks());
 	}
 	
 
