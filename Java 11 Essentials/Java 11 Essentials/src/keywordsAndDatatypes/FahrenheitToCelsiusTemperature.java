1a5f942d077b65f43ac3daa9fb8b71c36e0cb9b5
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-01-19 18:47:36 +0530

    Modified keywordsAndDatatypes package code files

--- Java 11 Essentials/Java 11 Essentials/src/keywordsAndDatatypes/FahrenheitToCelsiusTemperature.java
+++ Java 11 Essentials/Java 11 Essentials/src/keywordsAndDatatypes/FahrenheitToCelsiusTemperature.java
@@ -4,13 +4,14 @@ public class FahrenheitToCelsiusTemperature {
 	
     //The method converts temperature from one unit to another
 	public static void main(String[] args) {
+		
         double fahrenheit = 212.0;      // Input in Fahrenheit
 
-        double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);     // conversion from fahrenheit to celsius
+        double  celsius = ( ( 5 * (fahrenheit - 32.0) ) / 9.0);     // conversion from fahrenheit to celsius
         System.out.println(fahrenheit + " degree Fahrenheit is equal to " + celsius + " in Celsius");
         
-        //Write code here to convert temperature to Kelvin
-        double  kelvin = celsius + 273.15;     // conversion from celsius to kelvin
+        //Code to convert from Celsius to Kelvin
+        double  kelvin = celsius + 273.15;
         System.out.println(celsius + " degree Celsius is equal to " + kelvin + " in Kelvin");
 
 	}
