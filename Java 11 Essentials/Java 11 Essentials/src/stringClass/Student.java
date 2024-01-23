3f345a278ff1da5ef6fcfa5220b052e8d0138b92
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-01-23 19:49:53 +0530

    Modified stringClass package code files part-2

--- Java 11 Essentials/Java 11 Essentials/src/stringClass/Student.java
+++ Java 11 Essentials/Java 11 Essentials/src/stringClass/Student.java
@@ -2,27 +2,31 @@ package stringClass;
 
 public class Student {
 	
-//	Instance Variables
+//	Instance Variables 
 	private String firstName;
 	private String middleName;
 	private String lastName;
 	
 //	Methods
 	public Student change() {
-        this.setFirstName(this.firstName.trim());
-        this.setMiddleName(this.middleName.trim());
-        this.setLastName(this.lastName.trim());
-        if( this.middleName.isBlank() || this.middleName.isEmpty() ) {
+        this.setFirstName(this.getFirstName().trim());
+        this.setLastName(this.getLastName().trim());
+        if( this.getMiddleName().isBlank() ) {
         	this.setMiddleName("N.A.");
-        }
+        } else {
+        	this.setMiddleName(this.getMiddleName().trim());
+        } 
         return this;
 	}
 	public String generateInitials() {
-		Student student = this.change();
-		String str = student.getFirstName().substring(0,1) + student.getMiddleName().substring(0,1) + student.getLastName().substring(0,1);
-		return str;
+        Student student = this.change();
+        return student.getFirstName().split("")[0] + student.getMiddleName().split("")[0] + student.getLastName().split("")[0];
+//		return student.getFirstName().substring(0,1) + student.getMiddleName().substring(0,1) + student.getLastName().substring(0,1);
+//      return String.valueOf(this.getFirstName().charAt(0)) + 
+//         	   String.valueOf(this.getMiddleName().charAt(0)) +
+//         	   String.valueOf(this.getLastName().charAt(0));
 	}
-
+	
 //	Getters and Setters
 	public String getFirstName() {
 		return firstName;
--- Java 11 Essentials/Java 11 Essentials/src/stringClass/StudentTester.java
+++ Java 11 Essentials/Java 11 Essentials/src/stringClass/StudentTester.java
@@ -4,13 +4,12 @@ public class StudentTester {
 	
 	public static void main(String[] args) {
 		
-		Student std1=new Student();
-		std1.setFirstName("Filius  ");
-		std1.setMiddleName(" ");
-		std1.setLastName("Flitwich");
+		Student student = new Student();
+		student.setFirstName("Filius  ");
+		student.setMiddleName(" ");
+		student.setLastName("Flitwich");
 
-		System.out.println(std1.generateInitials());
-		
+		System.out.println(student.generateInitials());
 		
 	}
 
