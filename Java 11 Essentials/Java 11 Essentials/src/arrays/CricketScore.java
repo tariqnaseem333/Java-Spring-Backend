b6701f08e496d7505d9260a9ec15a5221ee0fef4
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-01-24 19:42:20 +0530

    Modified arrays package code files

--- Java 11 Essentials/Java 11 Essentials/src/arrays/CricketScore.java
+++ Java 11 Essentials/Java 11 Essentials/src/arrays/CricketScore.java
@@ -1,40 +1,38 @@
 package arrays;
 
 public class CricketScore {
-	
-public static void main(String a[]){
-        
-        //After implementing, copy the below code in JShell to execute
-        int scores[]={281, 344, 265, 272, 236, 324, 287};
-        double sum=0,avg=0;
-        
-        for(int index=0;index<scores.length;index++){
-            sum=sum+scores[index];
-        }
-       
-        avg=sum/scores.length;
-        
-        System.out.println("The average score of the team is "+avg);
-        //Complete the code as desired output shown in problem statement
-        
-        int aboveScore = 0;
-        int belowScore = 0;
-        int equalScore = 0;
-        for(int index=0;index<scores.length;index++){
-            if( scores[index] > avg ) {
-                aboveScore++;
-            }
-            else if( scores[index] < avg ) {
-                belowScore++;
-            }
-            else {
-                equalScore++;
-            }
-        }
-        
-        System.out.println("No. of matches having score above average is " + aboveScore);
-        System.out.println("No. of matches having score equal to average is " + equalScore);
-        System.out.println("No. of matches having score below average is " + belowScore);
-    }
+
+	public static void main(String[] args) {
+
+		int[] scores = {281, 344, 265, 272, 236, 324, 287};
+
+		double average = 0;
+		double sum = 0;
+		for( int i = 0; i < scores.length; i++ ) {
+			sum = sum + scores[i];
+		}
+
+		average = sum / scores.length;
+
+		int averageScoreCount = 0;
+		int aboveAverageScoreCount = 0;
+		int belowAverageScoreCount = 0;
+		for( int score : scores ) {
+			if( score > average ) {
+				aboveAverageScoreCount++;
+			} else if( score < average ) {
+				belowAverageScoreCount++;
+			} else {
+				averageScoreCount++;
+			}
+		}
+
+		System.out.println("The average score of the team is " + average + " runs");
+		System.out.println("No. of matches having a score above average is " + aboveAverageScoreCount);
+		System.out.println("No. of matches having a score equal to average is " + averageScoreCount);
+		System.out.println("No. of matches having a score below average is " + belowAverageScoreCount);
+
+
+	}
 
 }
