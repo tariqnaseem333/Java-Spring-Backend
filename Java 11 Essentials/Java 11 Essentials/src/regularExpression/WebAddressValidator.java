284985aaf0723a5e25160103ea1ad4fbd499c450
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-02-01 18:05:41 +0530

    Modified regularExpression package code files

--- Java 11 Essentials/Java 11 Essentials/src/regularExpression/WebAddressValidator.java
+++ Java 11 Essentials/Java 11 Essentials/src/regularExpression/WebAddressValidator.java
@@ -3,12 +3,10 @@ package regularExpression;
 public class WebAddressValidator {
 
 	public static boolean isValidWebAddress(String webAddress){
-		String regex = "(https:\\/\\/|http:\\/\\/)(www\\.)?([A-Za-z0-9]+)\\.(com|org|net)";
-		if( webAddress.matches(regex) ) {
-			return true;
-		} else {
-			return false;
-		}
+		
+    	String regex = "(https:\\/\\/|http:\\/\\/)(www\\.)?([A-Za-z0-9]+)\\.(com|org|net)";
+        return webAddress.matches(regex);
+		
 	}
 	
 }
--- Java 11 Essentials/Java 11 Essentials/src/regularExpression/WebAddressValidatorTester.java
+++ Java 11 Essentials/Java 11 Essentials/src/regularExpression/WebAddressValidatorTester.java
@@ -3,12 +3,15 @@ package regularExpression;
 public class WebAddressValidatorTester {
 
 	public static void main(String[] args) {
-		String str = "http://www.infosys.com";
-		if( WebAddressValidator.isValidWebAddress(str) ) {
+		
+		String webAddress = "http://www.infosys.com";
+		
+		if( WebAddressValidator.isValidWebAddress(webAddress) ) {
 			System.out.println("You have entered a valid web address");
 		} else {
 			System.out.println("You have entered an invalid web address");
 		}
+		
 	}
 
 }
