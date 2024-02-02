d4d31fede4981c402e9ad4b35f8f7131ef4d0ef5
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-02-02 18:50:42 +0530

    Modified staticKeyword package code files

--- Java 11 Essentials/Java 11 Essentials/src/staticKeyword/Car.java
+++ Java 11 Essentials/Java 11 Essentials/src/staticKeyword/Car.java
@@ -1,23 +1,30 @@
 package staticKeyword;
 
 public class Car {
-    private String color;
-	private static int numberOfCars = 0;    //static variable
 	
+//	Instance Variable
+    private String color;
+    
+//  Static Variable
+	private static int numberOfCars = 0;
+	
+//	Constructor
 	public Car(String color) {
-	   numberOfCars++;
 	   this.color = color;
+	   numberOfCars++;
 	}
 	
+//	Method
+	public void displayColor() {
+		System.out.println("Color of the car : " + this.color);
+	}
+	
+//	Getters
 	public String getColor() {       //static method
 	    return this.color;
 	}
-	
 	public static int getNumberOfCars() {
 		return numberOfCars;
 	}
-
-	public void displayColor() {
-		System.out.println("Color of the car : "+this.color);
-	}
+	
 }
--- Java 11 Essentials/Java 11 Essentials/src/staticKeyword/CarTester.java
+++ Java 11 Essentials/Java 11 Essentials/src/staticKeyword/CarTester.java
@@ -4,19 +4,21 @@ public class CarTester {
 	
 	public static void displayCar() {
 	    System.out.println("Displaying Car details");
+	    System.out.println("Number of cars created: " + Car.getNumberOfCars());
 	}
 	
 	public static void main(String[] args) {
+		
 		Car c1 = new Car("Red");
 		Car c2 = new Car("Green");
 		Car c3 = new Car("Blue");
-
+		
 		CarTester.displayCar();
-        System.out.println("Number of Cars created: " + Car.getNumberOfCars());
-        System.out.println("Colors of the Cars: ");
-        c1.displayColor();
-        c2.displayColor();
-        c3.displayColor();
+		
+	    System.out.println("Color of the cars:");
+		c1.displayColor();
+		c2.displayColor();
+		c3.displayColor();
 		
 	}
 
