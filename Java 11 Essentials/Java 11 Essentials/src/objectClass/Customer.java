a96224831645aaeec8f0f345a5949cf6f8fd7158
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-02-04 10:22:10 +0530

    Modified objectClass package code files

--- Java 11 Essentials/Java 11 Essentials/src/objectClass/Customer.java
+++ Java 11 Essentials/Java 11 Essentials/src/objectClass/Customer.java
@@ -1,35 +1,33 @@
 package objectClass;
 
 public class Customer {
-	
-//	Instance Variables
-	 private int customerId;
-	 private String name;
-	 private String phone;
-	 
-//	 Constructor
-	 public Customer(int id, String name, String phone) {
-		 this.customerId = id;
-		 this.name = name;
-		 this.phone = phone;
-	 }
-	 
-//	 Methods
-	 public String toString() {
-		return "Customer-" + this.name + " with ID-" + this.customerId;
-	 }
-	 
-	 public boolean equals( Object obj ) {
-		 Customer customer = ( Customer )obj;
-		 if( this.name.equals( customer.name ) && this.customerId == customer.customerId ) {
-			 return true;
-		 }
-		 return false;
-	 }
-	 
-	 public int hashCode() {
-		 Integer id = this.customerId;
-		 return 1 + this.name.hashCode() + id.hashCode();
-	 }
 
+//	Instance Variables
+	private int customerId;
+	private String name;
+	private String phone;
+
+//	 Constructor
+	public Customer(int id, String name, String phone) {
+		this.customerId = id;
+		this.name = name;
+		this.phone = phone;
 	}
+
+//	 Methods
+	public String toString() {
+//		Output Format : Customer-Max with ID-105
+		return "Customer-" + this.name + " with ID-" + this.customerId;
+	}
+
+	public boolean equals( Object obj ) {
+		Customer customer = ( Customer )obj;
+		return ( this.name.equals( customer.name ) && this.customerId == customer.customerId );
+	}
+
+	public int hashCode() {
+		Integer id = this.customerId;
+		return 1 + this.name.hashCode() + id.hashCode();
+	}
+
+}
--- Java 11 Essentials/Java 11 Essentials/src/objectClass/CustomerTester.java
+++ Java 11 Essentials/Java 11 Essentials/src/objectClass/CustomerTester.java
@@ -8,21 +8,19 @@ public class CustomerTester {
 	     Customer c2 = new Customer(109, "Nick", "9453281756");
 	     Customer c3 = new Customer(109, "Nick", "9453281756");
 	     
-	     if( c1.equals(c2) && c2.equals(c3) ) {
+//	     Printing only Unique Objects
+	     if( c1.equals(c2) ) {
+	    	 if( !c2.equals(c3) ) {
+	    		 System.out.println(c3);
+	    	 }
+	     } else if( c2.equals(c3) ) {
 	    	 System.out.println(c1);
-	     }
-	     else if( c1.equals(c2) ) {
+	     } else if( c1.equals(c3) ) {
+	    	 System.out.println(c2);
+	     } else {
 	    	 System.out.println(c1);
+	    	 System.out.println(c2);
 	    	 System.out.println(c3);
-	     }
-	     else if( c2.equals(c3) ) {
-	    	 System.out.println(c1);
-	    	 System.out.println(c2);
-	     }
-	     else {
-	    	 System.out.println(c1);
-	    	 System.out.println(c2);
-		     System.out.println(c3);
 	     }
 	    
 	 }
