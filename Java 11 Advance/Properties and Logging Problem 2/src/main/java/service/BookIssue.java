9db788eae37fa9de49110b4dbe6ccdc55151a3f5
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-04-18 20:12:50 +0530

    Solve 2nd Problem on Properties and Logging concept part-2

--- Java 11 Advance/Properties and Logging Problem 2/src/main/java/service/BookIssue.java
+++ Java 11 Advance/Properties and Logging Problem 2/src/main/java/service/BookIssue.java
@@ -1,11 +1,42 @@
 package service;
 
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.builder.fluent.Configurations;
+import org.apache.commons.configuration2.ex.ConfigurationException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 import com.infy.exception.InfyAcademyException;
 
 public class BookIssue {
 	
-	public void bookDetails(String bookName) throws InfyAcademyException {
-		//will implement this method
+	private static final Log LOGGER = LogFactory.getLog(BookIssue.class);
+	
+	public void bookDetails(String bookName) throws InfyAcademyException, ConfigurationException {
+
+		Configurations configurations = new Configurations();
+		PropertiesConfiguration config = configurations.properties("configuration.properties");	
+
+		try {
+			if(bookName.equals(config.getProperty("B1_NAME"))) {
+				if( Integer.parseInt((String)config.getProperty("B1_VALUE")) > 0)
+					LOGGER.info(config.getProperty("BOOK_AVAILABLE"));
+				else 
+					throw new InfyAcademyException("BOOK_UNAVAILABLE");
+			}
+			else if(bookName.equals(config.getProperty("B2_NAME"))) {
+				if( Integer.parseInt((String)config.getProperty("B2_VALUE")) > 0) 
+					LOGGER.info(config.getProperty("BOOK_AVAILABLE"));
+				else 
+					throw new InfyAcademyException("BOOK_UNAVAILABLE");
+			}
+			else {
+				throw new InfyAcademyException("INVALID_BOOKNAME");
+			}
+		} catch (InfyAcademyException e) {
+			LOGGER.error(config.getProperty(e.getMessage()), e);
+			throw e;
+		}
 		
 	}
 
--- Java 11 Advance/Properties and Logging Problem 2/src/main/java/userinterface/Tester.java
+++ Java 11 Advance/Properties and Logging Problem 2/src/main/java/userinterface/Tester.java
@@ -5,9 +5,21 @@ import com.infy.exception.InfyAcademyException;
 import service.BookIssue;
 
 public class Tester {
+	
 	public static void main(String[] args) throws InfyAcademyException {
-//		 will implement this method
+		
+		try {
+			BookIssue b1 = new BookIssue(); 
+			b1.bookDetails("The Book Thief");
+			BookIssue b2 = new BookIssue(); 
+			b2.bookDetails("The Kite Runner");
+			BookIssue b3 = new BookIssue(); 
+			b3.bookDetails("Greek Goddesses"); 
 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
