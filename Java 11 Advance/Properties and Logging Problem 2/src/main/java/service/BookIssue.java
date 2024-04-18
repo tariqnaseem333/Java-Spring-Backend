6a737ed53524cca02da04b84ec7b181d93b15c63
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-04-18 20:06:36 +0530

    Solve 2nd Problem on Properties and Logging concept part-1

---
+++ Java 11 Advance/Properties and Logging Problem 2/src/main/java/service/BookIssue.java
@@ -0,0 +1,12 @@
+package service;
+
+import com.infy.exception.InfyAcademyException;
+
+public class BookIssue {
+	
+	public void bookDetails(String bookName) throws InfyAcademyException {
+		//will implement this method
+		
+	}
+
+}
---
+++ Java 11 Advance/Properties and Logging Problem 2/src/main/java/userinterface/Tester.java
@@ -0,0 +1,13 @@
+package userinterface;
+
+import com.infy.exception.InfyAcademyException;
+
+import service.BookIssue;
+
+public class Tester {
+	public static void main(String[] args) throws InfyAcademyException {
+//		 will implement this method
+
+	}
+
+}
