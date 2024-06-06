924e457f30806fc1142cb5011a4b0d47e85a4a4c
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-06-06 19:54:48 +0530

    Solve 1 Problem on JUnit concept part-1

---
+++ Java 11 Advance/JUnit Problem 1/src/test/EmailValidatorTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import com.infy.exception.InfyAcademyException;
+
+public class EmailValidatorTest {
+	
+	public void validateEmailIdValidEmailId() throws InfyAcademyException {
+		//will implement this method
+	}
+
+	public void validateEmailIdInvalidEmailId() throws InfyAcademyException {
+		//will implement this method
+	}
+
+	public void validateEmailIdInvalidEmailIdException() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+	public void validateEmailIdParamterizedEmailId() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+}
---
+++ Java 11 Advance/JUnit Problem 1/src/test/PasswordValidatorTest.java
@@ -0,0 +1,23 @@
+package test;
+
+import com.infy.exception.InfyAcademyException;
+
+public class PasswordValidatorTest {
+	
+	public void validatePasswordValidPassword() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+	public void validatePasswordInvalidPassword() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+	public void validatePasswordInvalidPasswordException() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+	public void validatePasswordParameterizedPassword() throws InfyAcademyException {
+		//will implement this method
+	}
+	
+}
