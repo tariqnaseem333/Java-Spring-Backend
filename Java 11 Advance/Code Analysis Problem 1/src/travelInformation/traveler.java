ebc9418a21a97c5a975405714c44ad8a8549af0b
tariqnaseem333 <tariqnaseem333@gmail.com>
2024-06-07 19:29:27 +0530

    Learn Code Analysis concept in Java and Solve 1 Problem on Code Analysis part-2

--- Java 11 Advance/Code Analysis Problem 1/src/travelInformation/traveler.java
+++ Java 11 Advance/Code Analysis Problem 1/src/travelInformation/traveler.java
@@ -1,15 +1,31 @@
-package travelInformation;
+package travelInformation; // code violation: package name should have lowercase alphabets
 
-import java.util.Calendar;
+import java.util.Calendar; // code violation: unused import
  
-public class traveler {
+public class traveler { // code violation: class name should follow Pascalcase naming convention
 	
-    private final int customerId = 1001;
+//	Instance Variables
+    private final int customerId = 1001; // code violation: final field should be static too
     private int travelerId;
-    private int BaggageAmount;
+    private int BaggageAmount; // code violation: variable name should follow camelcase naming convention
     private int expiryyear;
     private boolean nocStatus;
+    
+//  Method
+    public boolean CheckBaggage() { // code violation: variable name should follow camelcase naming convention
+        boolean ret = false;
+        if(this.BaggageAmount>=0 && this.BaggageAmount<=40) {
+            nocStatus = true;
+            ret = true;
+        }
+        else {
+            nocStatus = true;
+            System.out.println("Invalid baggage amount");
+        }
+        return ret;
+    }
  
+//  Getters and Setters
     public int getTravelerId() {
         return travelerId;
     }
@@ -38,16 +54,4 @@ public class traveler {
         return customerId;
     }
  
-    public boolean CheckBaggage() {
-        boolean ret = false;
-        if(this.BaggageAmount>=0 && this.BaggageAmount<=40) {
-            nocStatus = true;
-            ret = true;
-        }
-        else {
-            nocStatus = true;
-            System.out.println("Invalid baggage amount");
-        }
-        return ret;
-    }
 }
